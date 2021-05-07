4b03ae5d731ec814a3eb26e636c068a1fb96a165
Picenipedro <67984725+Picenipedro@users.noreply.github.com>
2021-05-07 18:21:30 +0200

    Add files via upload

---
+++ App.py
@@ -0,0 +1,20 @@
+App.py
+import pandas as pd
+import plotly.express as px
+
+import dash
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+server = app.server
+
+#---------------------------------------------
+
+
+
+
+
+
